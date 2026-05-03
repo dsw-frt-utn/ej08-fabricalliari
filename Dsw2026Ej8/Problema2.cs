9a9fee17f746205655ae87b277c04728c652f8cc
Fabrizzio Calliari <fabrocalliari@gmail.com>
2026-05-03 10:48:47 -0300

    Actualizo Prolema 2, resuelto.

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            var producto = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity <= 0 ? 0 : quantity,
+                Total = quantity <= 0 ? 0 : quantity * unitPrice
+            };
+
+            return $"{producto.Code}-{producto.Description}-{producto.Total}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -3,10 +3,13 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   //--------Pruebas Problema 1-------\\
             var helper = new ProductHelper();
             string etiqueta = helper.ObtenerEtiquetaProducto(1001, "Coca cola", 2000);
             Console.WriteLine(etiqueta);
+            //--------Pruebas Problema 2
+            var p2 = new Problema2();
+            Console.WriteLine(p2.CrearResumenVenta(1000,"Pepsi Black",2,300m));
         }
     }
 }
